7c001703c7f8435d11a7b804a0a533af804dec86
jungai <juworaphol125@gmail.com>
2021-12-24 00:37:13 +0700

    lint happy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import GlobalStyles from '../styles/GlobalStyle'
-import type { AppProps } from 'next/app'
+import GlobalStyles from '../styles/GlobalStyle';
+import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <GlobalStyles />
     <Component {...pageProps} />
   </>
-)
+);
 
-export default App
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import 'twin.macro'
-import type { NextPage } from 'next'
+import 'twin.macro';
+import type { NextPage } from 'next';
 
 const Index: NextPage = () => {
   return (
     <div tw="h-screen flex items-center justify-center text-red-400">
       <h1>Hello</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
