e5f2ecc3c024a6e1ff79818cce13810d428b31f8
jungai <juworaphol125@gmail.com>
2021-12-24 00:02:13 +0700

    add twin.macro

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
+import GlobalStyles from '../styles/GlobalStyle'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
+)
 
-export default MyApp
+export default App
